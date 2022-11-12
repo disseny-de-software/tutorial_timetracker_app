3da341e1a8659638f46db9e201ef42048297ae44
joan-s <joans@cvc.uab.es>
2022-11-12 22:43:07 +0100

    revisio i instruccions per dartpad.dev

---
+++ step1.dart
@@ -0,0 +1,206 @@
+// see Serializing JSON inside model classes in
+// https://flutter.dev/docs/development/data-and-backend/json
+
+import 'package:intl/intl.dart';
+import 'dart:convert' as convert;
+import 'package:flutter/material.dart';
+
+final DateFormat _dateFormatter = DateFormat("yyyy-MM-dd HH:mm:ss");
+
+abstract class Activity {
+  late int id;
+  late String name;
+  DateTime? initialDate;
+  DateTime? finalDate;
+  late int duration;
+  List<dynamic> children = List<dynamic>.empty(growable: true);
+
+  Activity.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'],
+        initialDate = json['initialDate']==null ? null : _dateFormatter.parse(json['initialDate']),
+        finalDate = json['finalDate']==null ? null : _dateFormatter.parse(json['finalDate']),
+        duration = json['duration'];
+}
+
+
+class Project extends Activity {
+  Project.fromJson(Map<String, dynamic> json) : super.fromJson(json) {
+    if (json.containsKey('activities')) {
+      // json has only 1 level because depth=1 or 0 in time_tracker
+      for (Map<String, dynamic> jsonChild in json['activities']) {
+        if (jsonChild['class'] == "project") {
+          children.add(Project.fromJson(jsonChild));
+          // condition on key avoids infinite recursion
+        } else if (jsonChild['class'] == "task") {
+          children.add(Task.fromJson(jsonChild));
+        } else {
+          assert(false);
+        }
+      }
+    }
+  }
+}
+
+
+class Task extends Activity {
+  late bool active;
+  Task.fromJson(Map<String, dynamic> json) : super.fromJson(json) {
+    active = json['active'];
+    for (Map<String, dynamic> jsonChild in json['intervals']) {
+      children.add(Interval.fromJson(jsonChild));
+    }
+  }
+}
+
+
+class Interval {
+  late int id;
+  DateTime? initialDate;
+  DateTime? finalDate;
+  late int duration;
+  late bool active;
+
+  Interval.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        initialDate = json['initialDate']==null ? null : _dateFormatter.parse(json['initialDate']),
+        finalDate = json['finalDate']==null ? null : _dateFormatter.parse(json['finalDate']),
+        duration = json['duration'],
+        active = json['active'];
+}
+
+
+class Tree {
+  late Activity root;
+
+  Tree(Map<String, dynamic> dec) {
+    // 1 level tree, root and children only, root is either Project or Task. If Project
+    // children are Project or Task, that is, Activity. If root is Task, children are Instance.
+    if (dec['class'] == "project") {
+      root = Project.fromJson(dec);
+    } else if (dec['class'] == "task") {
+      root = Task.fromJson(dec);
+    } else {
+      assert(false);
+    }
+  }
+}
+
+
+Tree getTree() {
+  String strJson = "{"
+      "\"name\":\"root\", \"class\":\"project\", \"id\":0, \"initialDate\":\"2020-09-22 16:04:56\", \"finalDate\":\"2020-09-22 16:05:22\", \"duration\":26,"
+      "\"activities\": [ "
+      "{ \"name\":\"software design\", \"class\":\"project\", \"id\":1, \"initialDate\":\"2020-09-22 16:05:04\", \"finalDate\":\"2020-09-22 16:05:16\", \"duration\":16 },"
+      "{ \"name\":\"software testing\", \"class\":\"project\", \"id\":2, \"initialDate\": null, \"finalDate\":null, \"duration\":0 },"
+      "{ \"name\":\"databases\", \"class\":\"project\", \"id\":3,  \"finalDate\":null, \"initialDate\":null, \"duration\":0 },"
+      "{ \"name\":\"transportation\", \"class\":\"task\", \"id\":6, \"active\":false, \"initialDate\":\"2020-09-22 16:04:56\", \"finalDate\":\"2020-09-22 16:05:22\", \"duration\":10, \"intervals\":[] }"
+      "] "
+      "}";
+  Map<String, dynamic> decoded = convert.jsonDecode(strJson);
+  Tree tree = Tree(decoded);
+  return tree;
+}
+
+testLoadTree() {
+  Tree tree = getTree();
+  print("root name ${tree.root.name}, duration ${tree.root.duration}");
+  for (Activity act in tree.root.children) {
+    print("child name ${act.name}, duration ${act.duration}");
+  }
+}
+
+
+//void main() {
+//  testLoadTree();
+//}
+
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TimeTracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: const TextTheme(
+            subtitle1: TextStyle(fontSize:20.0),
+            bodyText2:TextStyle(fontSize:20.0)),
+      ),
+      home: PageActivities(),
+    );
+  }
+}
+
+
+class PageActivities extends StatefulWidget {
+  @override
+  _PageActivitiesState createState() => _PageActivitiesState();
+}
+
+class _PageActivitiesState extends State<PageActivities> {
+  late Tree tree;
+
+  @override
+  void initState() {
+    super.initState();
+    tree = getTree();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(tree.root.name),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.home),
+              onPressed: () {}
+            // TODO go home page = root
+          ),
+          //TODO other actions
+        ],
+      ),
+      body: ListView.separated(
+        // it's like ListView.builder() but better
+        // because it includes a separator between items
+        padding: const EdgeInsets.all(16.0),
+        itemCount: tree.root.children.length,
+        itemBuilder: (BuildContext context, int index) =>
+            _buildRow(tree.root.children[index], index),
+        separatorBuilder: (BuildContext context, int index) =>
+        const Divider(),
+      ),
+    );
+  }
+
+  Widget _buildRow(Activity activity, int index) {
+    String strDuration = Duration(seconds: activity.duration).toString().split('.').first;
+    // split by '.' and taking first element of resulting list
+    // removes the microseconds part
+    assert (activity is Project || activity is Task);
+    if (activity is Project) {
+      return ListTile(
+        title: Text('${activity.name}'),
+        trailing: Text('$strDuration'),
+        onTap: () => {},
+        // TODO, navigate down to show children tasks and projects
+      );
+    } else {
+      Task task = activity as Task;
+      Widget trailing;
+      trailing = Text('$strDuration');
+      return ListTile(
+        title: Text('${activity.name}'),
+        trailing: trailing,
+        onTap: () => {},
+        // TODO, navigate down to show intervals
+        onLongPress: () {},
+        // TODO start/stop counting the time for this task
+      );
+    }
+  }
+}
